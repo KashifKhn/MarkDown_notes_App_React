d728c3a3d9cd8be5929bf8859f317de65d243f72
Kashif Khan <KashifKhnx04@gmail.com>
2023-09-21 00:25:24 +0500

    Add note summary titles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,13 +4,16 @@ export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
+
+                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
+                    }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">
+                    {
+                        note.body === "" ? `Notes ${index}` : note.body.split(/\r|\n/g)[0]
+                    }
+                </h4>
             </div>
         </div>
     ))
