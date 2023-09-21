698351f9810c1bd9d042b6f8e4fcc6f04630979f
Kashif Khan <KashifKhnx04@gmail.com>
2023-09-21 14:48:09 +0500

    fixing the code also optimise the code

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import React from "react"
 import { useEffect, useState } from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import { nanoid } from "nanoid"
 
 export default function App() {
+
   const [notes, setNotes] = useState(
     () => JSON.parse(localStorage.getItem("notes")) || []
   )
+
   const [currentNoteId, setCurrentNoteId] = useState(
-    (notes[0] && notes[0].id) || ""
+    (notes[0]?.id) || ""
   )
+
+  const currentNote = notes.find(note => note.id === currentNoteId) || notes[0]
 
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes))
   }, [notes])
-
 
   function createNewNote() {
     const newNote = {
@@ -56,11 +58,11 @@ export default function App() {
     setNotes(oldNotes => oldNotes.filter(note => note.id != noteId))
   }
 
-  function findCurrentNote() {
-    return notes.find(note => {
-      return note.id === currentNoteId
-    }) || notes[0]
-  }
+  // function findCurrentNote() {
+  //   return notes.find(note => {
+  //     return note.id === currentNoteId
+  //   }) || notes[0]
+  // }
 
   return (
     <main>
@@ -74,16 +76,16 @@ export default function App() {
           >
             <Sidebar
               notes={notes}
-              currentNote={findCurrentNote()}
+              currentNote={currentNote}
               setCurrentNoteId={setCurrentNoteId}
               newNote={createNewNote}
-              deleteNote = {deleteNote}
+              deleteNote={deleteNote}
             />
             {
               currentNoteId &&
               notes.length > 0 &&
               <Editor
-                currentNote={findCurrentNote()}
+                currentNote={currentNote}
                 updateNote={updateNote}
               />
             }
