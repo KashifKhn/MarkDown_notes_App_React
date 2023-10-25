96e98294bd13b23ae2f315d620bd3c2e80ae87c3
Kashif Khan <KashifKhnx04@gmail.com>
2023-10-25 20:44:44 +0500

    fixing firebase Security Rules

--- src/firebase.js
+++ src/firebase.js
@@ -5,9 +5,9 @@ const firebaseConfig = {
     apiKey: "AIzaSyDkxUn4GMocrQNTGdzw8lvMDaQvW-6oRug",
     authDomain: "react-mark-down-notes.firebaseapp.com",
     projectId: "react-mark-down-notes",
+    appId: "1:170738936225:web:8a255701076b7d366983e0",
     storageBucket: "react-mark-down-notes.appspot.com",
     messagingSenderId: "170738936225",
-    appId: "1:170738936225:web:8a255701076b7d366983e0"
 };
 
 const app = initializeApp(firebaseConfig);
